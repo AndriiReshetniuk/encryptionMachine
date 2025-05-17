523fb69c3fa4e0d72882807f1e02e4b8f85b6eaa
AndriiReshetniuk <andriyreshet@gmail.com>
2025-05-17 17:40:56 +0300

    Добавьте файлы проекта.

---
+++ Encryptor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encryptionMachine
+{
+    public class Encryptor
+    {
+        public string ceazarEncryption(string wordToEncryp)
+        {
+            StringBuilder encryptedWord = new StringBuilder();
+            for(int i = 0; i< wordToEncryp.Length; i++)
+            {
+                char encryptedSymbol = (char)(wordToEncryp[i] + 1);
+                encryptedWord.Append(encryptedSymbol);
+            }
+            return encryptedWord.ToString();
+        }
+
+        public string caezarDecryption(string wordToDecrypt)
+        {
+            StringBuilder decryptedWord = new StringBuilder();
+            for(int i = 0; i < wordToDecrypt.Length; i++)
+            {
+                char decryptedSymbol = (char)(wordToDecrypt[i] - 1);
+                decryptedWord.Append(decryptedSymbol);
+            }
+            return decryptedWord.ToString();
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace encryptionMachine
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Encryptor encryptor;
+        public MainWindow()
+        {
+            InitializeComponent();
+            encryptor = new Encryptor();
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            userInput.Text = encryptor.ceazarEncryption(userInput.Text);
+        }
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            userInput.Text = encryptor.caezarDecryption(userInput.Text);
+        }
+    }
+}
